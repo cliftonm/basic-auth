a0836b841de3d10237a51088b07216ece0742434
Marc Clifton <marc.clifton@gmail.com>
2013-04-06 19:38:23 -0400

    Initial push.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicAuth::Application.config.secret_token = '9c26da72b8b2ea23ed1a6fa35485f87046599c1ebe5a9df1347c30f0a504f98f4d54e868b5e7552ebfefb60c2a14cbbee8818dc4e42316ad90ee424bf54420b1'
